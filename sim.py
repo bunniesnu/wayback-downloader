0a6d5cab0e344ca8d7428b521549a535bd0d4ee5
Bunniesnu <kimin.kwon@snu.ac.kr>
2025-06-08 15:39:55 +0900

    add simulation with selenium & auto download assets

---
+++ sim.py
@@ -0,0 +1,59 @@
+import json
+from selenium import webdriver
+import time
+from sys import argv
+import requests
+from pathlib import Path
+
+if len(argv) < 4:
+    print("Usage: python sim.py <timestamp> <url> <local_url>")
+    exit(1)
+TIMESTAMP = argv[1]
+URL = argv[2]
+LOCAL_URL = argv[3]
+
+def handle_404(url: str):
+    endpoint = url.replace(LOCAL_URL, '')
+    print(endpoint)
+    path = Path("output") / TIMESTAMP / endpoint.lstrip('/')
+    path.parent.mkdir(parents=True, exist_ok=True)
+    retry = 0
+    while retry < 3:
+        try:
+            download_url = f"https://web.archive.org/web/{TIMESTAMP}id_/{URL}{endpoint}"
+            response = requests.get(download_url)
+            if response.status_code == 200 or response.status_code == 206:
+                path.write_bytes(response.content)
+                print(f"Downloaded {url} at {TIMESTAMP} to {path}")
+                break
+            elif response.status_code == 404:
+                print(f"404 Not Found for {url} at {TIMESTAMP}")
+                break
+            else:
+                retry += 1
+                if retry >= 3:
+                    print(f"Failed to download {download_url}: {response.status_code}")
+                    break
+        except Exception as e:
+            retry += 1
+            if retry >= 3:
+                raise e
+        time.sleep(10)
+
+options = webdriver.ChromeOptions() 
+options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+driver = webdriver.Chrome(options=options)
+
+driver.get(LOCAL_URL)
+
+try:
+    while True:
+        for entry in driver.get_log('performance'):
+            message = json.loads(entry['message'])['message']
+            if message.get('method') == 'Network.responseReceived':
+                resp = message['params']['response']
+                if resp.get('status') == 404:
+                    handle_404(resp['url'])
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Monitoring stopped by user.")
